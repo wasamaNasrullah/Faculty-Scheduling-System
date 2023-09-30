41e843dc9a683c285a57daef996593af3ba8f261
wasamaNasrullah <145775436+wasamaNasrullah@users.noreply.github.com>
2023-09-30 14:09:21 -0700

    Add files via upload

---
+++ topbar wasama v1.php
@@ -0,0 +1,27 @@
+<style>
+	.logo {
+    margin: auto;
+    font-size: 20px;
+    background: white;
+    padding: 7px 11px;
+    border-radius: 50% 50%;
+    color: #000000b3;
+}
+</style>
+
+<nav class="navbar navbar-light fixed-top bg-primary" style="padding:0;min-height: 3.5rem">
+  <div class="container-fluid mt-2 mb-2">
+  	<div class="col-lg-12">
+  		<div class="col-md-1 float-left" style="display: flex;">
+  		
+  		</div>
+      <div class="col-md-4 float-left text-white">
+        <large><b> Faculty Scheduling System</b></large>
+      </div>
+	  	
+        </div>
+      </div>
+  </div>
+  
+</nav>
+
